94bd0eedf54b7bb4206ad5c42e83dc3b86431c57
rypetrat <rypetrat@bu.edu>
2025-04-02 13:45:44 -0400

    small edit

--- app/stock-details/page.tsx
+++ app/stock-details/page.tsx
@@ -27,27 +27,27 @@ const StyledB2 = styled.b<{ value: number }>`
 `;
 
 export default function StockDetailsPage() {
-    const { stockData } = useStock();
-    const symbol = stockData.length > 0 ? stockData[0].symbol : '';
-    const router = useRouter();
-    const delta = stockData.length > 0 ? stockData[0].close - stockData[29].close : 0;
-    const deltaPercent = stockData.length > 0 ? (delta / stockData[29].close) * 100 : 0;
-    
-    if (stockData.length === 0) {
-      return (
-        <div style={{textAlign: "center", padding: "20px"}}>
-          <p>No stock data available.</p>
-          <StyledButton onClick={() => router.push('/')}>Back to Home</StyledButton>
-        </div>
-      );
-    }
-
+  const { stockData } = useStock();
+  const symbol = stockData.length > 0 ? stockData[0].symbol : '';
+  const router = useRouter();
+  const delta = stockData.length > 0 ? stockData[0].close - stockData[29].close : 0;
+  const deltaPercent = stockData.length > 0 ? (delta / stockData[29].close) * 100 : 0;
+  
+  if (stockData.length === 0) {
     return (
-      <div style={{textAlign: "center", display: "flex", flexDirection: "column", alignItems: "center", padding: "20px"}}>
-        <h2>Stock price delta since {new Date(stockData[29].date).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' })}: <StyledB2 value={delta}>${delta.toFixed(2)} ({deltaPercent.toFixed(2)}%)</StyledB2></h2>
-        <h1>Last 30 Days of Stock Details for <StyledB>{symbol}</StyledB></h1>
-        {stockData.length > 0 ? (stockData.map((stock, index) => (<StockPreview key={index} stock={stock}/>))) : (<p>No stock data available.</p>)}
+      <div style={{textAlign: "center", padding: "20px"}}>
+        <p>No stock data available.</p>
         <StyledButton onClick={() => router.push('/')}>Back to Home</StyledButton>
       </div>
     );
+  }
+
+  return (
+    <div style={{textAlign: "center", display: "flex", flexDirection: "column", alignItems: "center", padding: "20px"}}>
+      <h2>Stock price delta since {new Date(stockData[29].date).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' })}: <StyledB2 value={delta}>${delta.toFixed(2)} ({deltaPercent.toFixed(2)}%)</StyledB2></h2>
+      <h1>Last 30 Days of Stock Details for <StyledB>{symbol}</StyledB></h1>
+      {stockData.length > 0 ? (stockData.map((stock, index) => (<StockPreview key={index} stock={stock}/>))) : (<p>No stock data available.</p>)}
+      <StyledButton onClick={() => router.push('/')}>Back to Home</StyledButton>
+    </div>
+  );
 }
